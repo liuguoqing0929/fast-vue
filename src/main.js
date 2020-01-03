b12ff56d8ed6c81a7ae021edc919978c5dc61bc7
liuguoqing0929 <302650374@qq.com>
2020-01-03 15:47:08 +0800

    添加echarts的使用

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 import httpRequest from './request/http'
+import echarts from 'echarts'
 
+Vue.prototype.$echarts = echarts
 Vue.prototype.$http = httpRequest
-
 Vue.prototype.$axios = axios // 全局注册，使用方法为:this.$axios
 // eslint-disable-next-line no-undef
 // Vue.prototype.qs = qs // 全局注册，使用方法为:this.qs
--- src/view/module/test-module.vue
+++ src/view/module/test-module.vue
@@ -10,6 +10,7 @@
       <div>
         测试的自定义组件
       </div>
+      <div style="width: 300px; height: 300px" ref="chart"></div>
     </div>
 </template>
 
@@ -26,7 +27,27 @@ export default {
       tempAgeAttr: this.ageAttr
     }
   },
+  mounted () {
+    this.initChat()
+  },
   methods: {
+    initChat: function () {
+      let myChart = this.$echarts.init(this.$refs.chart)
+      console.log(this.$refs.chart)
+      myChart.setOption({
+        title: { text: '在Vue中使用echarts' },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      })
+    },
     login: function () {
       console.log('child')
     }
